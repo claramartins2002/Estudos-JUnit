fcd76a259a3dd3b5d9105b7daefd8deacebd4457
claramartins2002 <claramartins.cm57@gmail.com>
2021-12-17 12:03:06 -0300

    anotações e testes com parâmetros

--- src/main/java/com/example/TestesUnitarios/teste/TesteJUnit.java
+++ src/main/java/com/example/TestesUnitarios/teste/TesteJUnit.java
@@ -3,6 +3,10 @@ package com.example.TestesUnitarios.teste;
 import com.example.TestesUnitarios.classe.Calculadora;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -41,6 +45,21 @@ public class TesteJUnit {
         assertEquals(0, calc.soma(2,-2), 0.0);
     }
 
+    @DisplayName("Valida múltiplas somas com informações CSV")
+    @ParameterizedTest
+    @CsvSource({"1.0, 1.0, 2.0", "2.0, 3.0, 5.0"})
+       void validaMultiplasSomas(double parcela1, double parcela2, double resultado) {
+        Calculadora calc = new Calculadora();
+        assertEquals(resultado, calc.soma(parcela1, parcela2), 0);
+    }
+
+    @DisplayName("Valida múltiplas somas com informações CSV")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    void validaMultiplasSomasComArquivoExterno(double parcela1, double parcela2, double resultado) {
+        Calculadora calc = new Calculadora();
+        assertEquals(resultado, calc.soma(parcela1, parcela2), 0);
+    }
 
 
 }
